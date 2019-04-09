2bc6d5f16ff08a0c08abae5bd88f4605f700eed1
vupdh-0867 <phan.dang.hai.vu@sun-asterisk.com>
2019-04-09 09:46:23 +0700

    view courses page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,21 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery-3.3.1.min
+//= require popper
+//= require bootstrap.min
+//= require stellar
+//= require simpleLightbox.min
+//= require jquery.nice-select.min
+//= require imagesloaded.pkgd.min
+//= require isotope.pkgd.min
+//= require owl.carousel.min
+//= require jquery.ajaxchimp.min
+//= require jquery.waypoints.min
+//= require jquery.magnific-popup.min
+//= require jquery.counterup
+//= require mail-script
+//= require theme
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks

684bd17fd96949dac3ae2ddd947e54640908858a
Bui Lam Quang Ngoc <bui.lam.quang.ngoc@framgia.com>
2019-04-19 15:54:26 +0700

    Done task 10619

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
+    resources :users
     resources :courses, only: %i(index show)
   end
 end

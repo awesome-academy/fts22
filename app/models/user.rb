9281c66b980d1217634f45c361898bc4e98fc447
vupdh-0867 <phan.dang.hai.vu@sun-asterisk.com>
2019-05-13 10:55:42 +0700

    Replace authentication with gem devise part 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+    :recoverable, :rememberable, :validatable
   has_many :user_courses, dependent: :destroy
   has_many :course_supervisors, dependent: :destroy
   has_many :user_subjects, dependent: :destroy
@@ -7,47 +9,9 @@ class User < ApplicationRecord
   has_many :trainer_courses, through: :course_supervisors, source: :course
   has_many :subjects, through: :user_subjects
   has_many :tasks, through: :user_tasks
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  has_secure_password
-  validates :email, presence: true,
-    length: {maximum: Settings.app.models.user.email_max_length},
-    format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
-  validates :name, presence: true,
-    length: {maximum: Settings.app.models.user.name_max_length}
-  validates :password, presence: true,
-    length: {minimum: Settings.app.models.user.password_max_length},
-    allow_nil: true
   enum role: {trainee: 0, admin: 1, trainer: 2}
   scope :except_user_id, ->(user_id){where.not id: user_id}
   scope :newest, ->{order created_at: :desc}
-
-  def self.digest string
-    BCrypt::Password.create(string, cost: User.get_cost)
-  end
-
-  def self.get_cost
-    return BCrypt::Engine.min_cost if ActiveModel::SecurePassword.min_cost
-    BCrypt::Engine.cost
-  end
-
-  def self.new_token
-    SecureRandom.urlsafe_base64
-  end
-
-  def remember
-    self.remember_token = User.new_token
-    update remember_digest: User.digest(remember_token)
-  end
-
-  def authenticated? attribute, token
-    digest = send "#{attribute}_digest"
-    return false if digest.nil?
-    BCrypt::Password.new(digest).is_password? token
-  end
-
-  def forget
-    update remember_digest: nil
-  end
 
   def send_created_email
     UserMailer.user_created(self).deliver_later
---
+++ db/migrate/20190513013916_devise_create_users.rb
@@ -0,0 +1,22 @@
+class DeviseCreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      ## Database authenticatable
+      t.string :email, null: false, default: ""
+      t.string :name, null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+      t.integer :role, default: 0
+
+      ## Recoverable
+      t.string :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
+      t.datetime :remember_created_a
+      t.timestamps null: false
+    end
+
+    add_index :users, :email, unique: true
+    add_index :users, :reset_password_token, unique: true
+  end
+end
